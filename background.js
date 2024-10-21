0d7d05f502ad7a41ae33b903bb788757b770de82
Kacper Markiewicz <kacpermarkiewicz@protonmail.com>
2024-10-21 20:34:38 +0200

    Check website on tab change & load

--- background.js
+++ background.js
@@ -5,16 +5,40 @@ const subject = 'https://harmonogramy.dsw.edu.pl/Plany/PlanyPrzedmiotow';
 const course = 'https://harmonogramy.dsw.edu.pl/Plany/PlanyTokow';
 const prefixes = [group, leader, room, subject, course];
 
-chrome.action.onClicked.addListener(async (tab) =>
-{
-    if (prefixes.some(prefix => tab.url.startsWith(prefix)))
-    {
-        const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
-        const nextState = prevState === 'ON' ? 'OFF' : 'ON';
-        await chrome.action.setBadgeText(
-        {
-            tabId: tab.id,
-            text: nextState,
-        });
+function onLoadDSW(changeInfo, tab) {
+    if (changeInfo.status == 'complete' && prefixes.some(prefix => tab.url.startsWith(prefix))) {
+        return true;
     }
-});
+    else {
+        return false;
+    }
+}
+
+function onChangeDSW(givenPage) {
+    if (prefixes.some(prefix => givenPage.startsWith(prefix))) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+    if (!onLoadDSW(changeInfo, tab)) {
+        return;
+    }
+
+    // continue code here
+})
+
+chrome.tabs.onActivated.addListener(function (activeInfo) {
+    var currentPage;
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        currentPage = tabs[0].url;
+        if (!onChangeDSW(currentPage)) {
+            return;
+        }
+
+        // continue code here
+    });
+})
