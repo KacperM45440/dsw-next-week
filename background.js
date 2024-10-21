f3da715175c9cdafcd128fab14c31826af4997d9
Kacper Markiewicz <kacpermarkiewicz@protonmail.com>
2024-10-21 19:03:45 +0200

    Detecting course website

--- background.js
+++ background.js
@@ -1,5 +1,20 @@
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.action.setBadgeText({
-    text: "OFF",
-  });
+const group = 'https://harmonogramy.dsw.edu.pl/Plany/PlanyGrup';
+const leader = 'https://harmonogramy.dsw.edu.pl/Plany/PlanyProwadzacych';
+const room = 'https://harmonogramy.dsw.edu.pl/Plany/PlanySal';
+const subject = 'https://harmonogramy.dsw.edu.pl/Plany/PlanyPrzedmiotow';
+const course = 'https://harmonogramy.dsw.edu.pl/Plany/PlanyTokow';
+const prefixes = [group, leader, room, subject, course];
+
+chrome.action.onClicked.addListener(async (tab) =>
+{
+    if (prefixes.some(prefix => tab.url.startsWith(prefix)))
+    {
+        const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
+        const nextState = prevState === 'ON' ? 'OFF' : 'ON';
+        await chrome.action.setBadgeText(
+        {
+            tabId: tab.id,
+            text: nextState,
+        });
+    }
 });
