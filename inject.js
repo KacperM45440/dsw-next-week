e9aaf0be5d93ac02287190e779d4414671f33259
Kacper Markiewicz <kacpermarkiewicz@protonmail.com>
2024-12-03 00:43:18 +0100

    Removed tabs from permissions

--- inject.js
+++ inject.js
@@ -1,5 +1,6 @@
 const radioButton = document.getElementById("RadioList_Termin1.5");
 
+//todo: set cookies for RadioList_TerminT, RadioList_TerminSal, RadioList_TerminProw
 radioButton.onclick = function () {
     const [startDate, endDate] = radioButton.value.split('\\');
 
